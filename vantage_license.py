7715ad5cdb159838775e9cb5dd0d8791ed6a798e
MarceloAraujo-Abbyy <marcelo.araujo@abbyy.com>
2024-06-18 15:28:10 -0300

    Update vantage_license.py

--- vantage_license.py
+++ vantage_license.py
@@ -1,7 +1,7 @@
 
-#__import__('pysqlite3')
-#import sys
-#sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import sqlite3
 import streamlit as st

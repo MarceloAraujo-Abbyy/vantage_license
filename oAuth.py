6872b45c58d0ff4d046c12f092050b0d33ad77eb
MarceloAraujo-Abbyy <marcelo.araujo@abbyy.com>
2024-06-19 10:18:23 -0300

    Update oAuth.py

--- oAuth.py
+++ oAuth.py
@@ -27,7 +27,7 @@ def pkce_challenge_from_verifier(v):
 client_id = st.secrets["VANTAGE_CLIENT_ID"]
 client_secret = st.secrets["VANTAGE_SECRET_ID"]
 authorization_base_url = 'https://vantage-us.abbyy.com/auth2/connect/authorize'
-token_url = 'https://vantage-us.abbyy.com/auth2//v1/token'
+token_url = 'https://vantage-us.abbyy.com/auth2/v1/token'
 redirect_uri = 'https://vantageaccess.streamlit.app'  # This should be the same as set in your OAuth provider settings
 scope = "openid permissions global.wildcard offline_access"
 state = string_num_generator(20)
@@ -41,7 +41,23 @@ oauth = OAuth2Session(client_id, redirect_uri=redirect_uri)
 st.title("ABBYY Vantage OAuth2 Authentication")
 
 auth_link = "https://vantage-us.abbyy.com/auth2/connect/authorize?client_id="+client_id+"&redirect_uri="+redirect_uri+"&response_type=code&scope="+scope+"&state="+state+"&code_challenge="+challenger+"&code_challenge_method=S256&productId=a8548c9b-cb90-4c66-8567-d7372bb9b963"
-st.write("auth_link => "+ auth_link)
 
 authorization_url, state = oauth.authorization_url(auth_link)
 st.write(f"[Authorize with ABBYY Vantage]({authorization_url})")
+
+# Step 1: User clicks the authorization link
+if 'code' not in st.st.query_params():
+    authorization_url, state = oauth.authorization_url(authorization_base_url)
+    st.write(f"[Authorize with ABBYY Vantage]({authorization_url})")
+else:
+    # Step 2: User returns to the app with the authorization code
+    authorization_response = st.st.query_params()
+    authorization_response_url = f"{redirect_uri}?code={authorization_response['code'][0]}"
+
+    # Step 3: Fetch the token using the authorization response URL
+    token = oauth.fetch_token(token_url, authorization_response=authorization_response_url, client_secret=client_secret)
+
+    # Step 4: Use the token to access protected resources
+    st.write("Authentication successful!")
+    st.write(token)
+

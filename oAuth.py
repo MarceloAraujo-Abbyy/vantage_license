d45cee396b66ce32101e0dedceb46143f8b666e7
MarceloAraujo-Abbyy <marcelo.araujo@abbyy.com>
2024-06-19 11:51:39 -0300

    Update oAuth.py

--- oAuth.py
+++ oAuth.py
@@ -4,6 +4,7 @@ import random
 import hashlib
 import base64
 import requests 
+from urllib.parse import urlencode
 
 def string_num_generator(size):
     chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
@@ -34,7 +35,7 @@ product_id= "a8548c9b-cb90-4c66-8567-d7372bb9b963"
 state = string_num_generator(20)
 verifier = string_num_generator(56)
 challenger = pkce_challenge_from_verifier(verifier)
-auth_link = authorization_base_url+"?client_id="+client_id+"&redirect_uri="+redirect_uri+"&response_type=code&scope="+scope+"&state="+state+"&code_challenge="+challenger+"&code_challenge_method=S256&productId="+product_id
+auth_link = urlencode(authorization_base_url+"?client_id="+client_id+"&redirect_uri="+redirect_uri+"&response_type=code&scope="+scope+"&state="+state+"&code_challenge="+challenger+"&code_challenge_method=S256&productId="+product_id)
 
 st.title("ABBYY Vantage OAuth2 Authentication")
 st.write(auth_link)

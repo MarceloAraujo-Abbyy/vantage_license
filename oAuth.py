88756f0b05ac0b16c29c185bad6fba3a3e468297
MarceloAraujo-Abbyy <marcelo.araujo@abbyy.com>
2024-06-19 10:44:35 -0300

    Update oAuth.py

--- oAuth.py
+++ oAuth.py
@@ -3,6 +3,7 @@ import string
 import random
 import hashlib
 import base64
+import requests 
 from requests_oauthlib import OAuth2Session
 
 
@@ -27,7 +28,7 @@ def pkce_challenge_from_verifier(v):
 client_id = st.secrets["VANTAGE_CLIENT_ID"]
 client_secret = st.secrets["VANTAGE_SECRET_ID"]
 authorization_base_url = 'https://vantage-us.abbyy.com/auth2/connect/authorize'
-token_url = 'https://vantage-us.abbyy.com/auth2/v1/token'
+token_url = 'https://vantage-us.abbyy.com/auth2/connect/token'
 redirect_uri = 'https://vantageaccess.streamlit.app'  # This should be the same as set in your OAuth provider settings
 scope = "openid permissions global.wildcard offline_access"
 state = string_num_generator(20)
@@ -43,21 +44,36 @@ st.title("ABBYY Vantage OAuth2 Authentication")
 auth_link = "https://vantage-us.abbyy.com/auth2/connect/authorize?client_id="+client_id+"&redirect_uri="+redirect_uri+"&response_type=code&scope="+scope+"&state="+state+"&code_challenge="+challenger+"&code_challenge_method=S256&productId=a8548c9b-cb90-4c66-8567-d7372bb9b963"
 
 authorization_url, state = oauth.authorization_url(auth_link)
-st.write(f"[Authorize with ABBYY Vantage]({authorization_url})")
+
 
 # Step 1: User clicks the authorization link
 if 'code' not in st.query_params:
     authorization_url, state = oauth.authorization_url(authorization_base_url)
-    #st.write(f"[Authorize with ABBYY Vantage]({authorization_url})")
+    st.write(f"[Authorize with ABBYY Vantage]({authorization_url})")
 else:
     # Step 2: User returns to the app with the authorization code
     authorization_response = st.query_params
-    authorization_response_url = f"{redirect_uri}?code={authorization_response['code']}"
+    authorization_response_code = authorization_response['code']
+    st.write("authorization_response_code: " + authorization_response_code) 
 
-
-    st.write("authorization_response_url: " + authorization_response_url) 
-    
     # Step 3: Fetch the token using the authorization response URL
+    data = {
+        'code_verifier': verifier,
+        'grant_type': "authorization_code",
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'code': authorization_response_code,
+        'redirect_uri': redirect_uri,
+        'scope': scope
+    }
+    response = requests.post(token_url, data=data, headers={'accept': '*/*', 'Content-Type': 'application/x-www-form-urlencoded'})
+    if response.status_code == 200:
+        response_data = response.json()
+        token = response_data['access_token']
+    else:
+        st.write("Erro logging: " + response.text) 
+
+
     token = oauth.fetch_token(token_url, authorization_response=authorization_response_url, client_secret=client_secret)
 
     # Step 4: Use the token to access protected resources

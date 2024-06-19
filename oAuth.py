4c48d919c8aa5df68bb0fdbf6c5220998cd3da0f
MarceloAraujo-Abbyy <marcelo.araujo@abbyy.com>
2024-06-19 10:06:27 -0300

    Update oAuth.py

--- oAuth.py
+++ oAuth.py
@@ -40,7 +40,7 @@ oauth = OAuth2Session(client_id, redirect_uri=redirect_uri)
 # Streamlit app logic
 st.title("ABBYY Vantage OAuth2 Authentication")
 
-auth_link = "https://vantage-us.abbyy.com/auth2/connect/authorize?client_id="+client_id+"&redirect_uri="+redirect_uri+"&response_type=code&scope="+scope+"&state="+state+"&code_challenge"+challenger+"&code_challenge_method=S256&productId=a8548c9b-cb90-4c66-8567-d7372bb9b963"
+auth_link = "https://vantage-us.abbyy.com/auth2/connect/authorize?client_id="+client_id+"&redirect_uri="+redirect_uri+"&response_type=code&scope="+scope+"&state="+state+"&code_challenge="+challenger+"&code_challenge_method=S256&productId=a8548c9b-cb90-4c66-8567-d7372bb9b963"
 st.write("auth_link => "+ auth_link)
 
 authorization_url, state = oauth.authorization_url(auth_link)
